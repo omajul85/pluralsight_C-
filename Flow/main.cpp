f4d129745ec3f78dabc51d06d17920aeb5b3e64e
Omar Alvarez <omajul2@hotmail.com>
2016-07-26 15:33:04 +0100

    Add Flow of control examples

---
+++ Flow/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // IF
+
+    int x,y;
+    cout << "Enter two numbers:" << endl;
+    cin >> x >> y;
+
+    cout << x;
+    if(x > y) {
+        cout << " is larger than ";
+    } else if (x < y) {
+        cout << " is not larger than ";
+    } else {
+        cout << " is equal to ";
+    }
+    cout << y << endl;
+
+    // WHILE
+
+    int a;
+    int i(2);
+    bool prime = true;
+
+    cout << "Enter a number:" << endl;
+    cin >> a;
+
+    while (i < a/i){
+        int factor = a/i;
+        if (factor * i == a) {
+            cout << "factor found " << factor << endl;
+            prime = false;
+            break;
+        }
+        i++;
+    }
+    cout << a << " is ";
+    if (prime)
+        cout << "prime";
+    else
+        cout << "not prime";
+    cout << endl;
+
+    // FOR
+
+    int b;
+    bool primo = true;
+
+    cout << "Enter a number:" << endl;
+    cin >> b;
+
+    for (int i = 2; i < b/i; i++){
+        int factor = b/i;
+        if (factor * i == a) {
+            cout << "factor found " << factor << endl;
+            primo = false;
+            break;
+        }
+    }
+
+    cout << b << " is ";
+    if (primo)
+        cout << "prime";
+    else
+        cout << "not prime";
+    cout << endl;
+
+    // SWITCH
+    int n;
+    cout << "Enter a number, 0 to quit" << endl;
+    cin >> n;
+
+    while (n > 0){
+        switch(n){
+            case 1:
+                cout << "You entered 1" << endl;
+                break;
+            case 2:
+            case 3:
+                cout << "You entered 2 or 3" << endl;
+                break;
+            case 4:
+                cout << "You entered 4" << endl;
+                break;
+            default:
+                cout << "You entered other than 1-4" << endl;
+                break;
+        }
+        cout << "Enter a number, 0 to quit" << endl;
+        cin >> n;
+    }
+    return 0;
+}
